7fd2e7f30d211851f8d78612b5e92d9e83d28524
chris-oh99 <christopher.oh1999@gmail.com>
2021-09-24 13:51:03 -0500

    Super Final Paranoia Check

--- project-0/src/main/java/SBAutils/ConnMan.java
+++ project-0/src/main/java/SBAutils/ConnMan.java
@@ -2,6 +2,7 @@ package SBAutils;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.util.Properties;
 
@@ -11,10 +12,20 @@ public class ConnMan {
     private ConnMan() {
 
     }
-    public static Connection getConn() throws FileNotFoundException {
+    public static Connection getConn() throws IOException {
         if (conn == null) {
             Properties props = new Properties();
             FileReader connProp = new FileReader("src/main/resources/conn.properties");
+            props.load(connProp);
+
+            // "jdbc:mariadb://hostname:port/databaseName?user=username&password=password"
+
+            String connStr= "jdbc.mariadb://" +
+            props.getProperty("hostname") + ":" +
+            props.getProperty("port") + "/" +
+            props.getProperty("databaseName") + "?user=" +
+            props.getProperty("user") + "&password=" +
+            props.getProperty("password");
         }
         return conn;
     }
